21a91704ab74a6467741d8cb5891df6a012f6b7c
Barna Farago <bfarago@gmail.com>
2024-06-16 22:19:28 +0200

    v2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Swift_SetList",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v17),
+        .macOS(.v13)
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
